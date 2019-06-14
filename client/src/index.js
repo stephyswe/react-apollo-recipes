ab0fb41edab9aeb530ca21c8aa6adf80ecb7bd4a
Stephanie <stephaniiehallberg@gmail.com>
2019-06-14 22:05:11 +0200

    GraphQL and Apollo Setup

---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './Components/App';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4444/graphql',
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config({ path: 'variables.env' });
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
+// Bring in GraphQL-Express middleware
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers');
+
+// Create Schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB connected'))
+  .catch(error => console.error(error));
+
+// init application
+const app = express();
+
+// Create >GraphiQL application
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+// Connect schemas with GraphQL
+app.use(
+  '/graphql',
+  bodyParser.json(),
+  graphqlExpress({
+    schema,
+    context: {
+      Recipe,
+      User,
+    },
+  })
+);
+
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
