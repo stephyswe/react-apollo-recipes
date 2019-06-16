ccc072c95bfe1c8cb4c0876fb91500507dcdfc19
Stephanie <stephaniiehallberg@gmail.com>
2019-06-16 10:51:52 +0200

    SignUp Functionality

--- client/src/Components/queries/index.js
+++ client/src/Components/queries/index.js
@@ -1,14 +1,28 @@
-import {gql} from 'apollo-boost'
+import { gql } from 'apollo-boost';
 
+/* Recipes Queries */
 export const GET_ALL_RECIPES = gql`
-query {
-  getAllRecipes {
-    name
-    description
-    instructions
-    category
-    likes
-    createdDate
+  query {
+    getAllRecipes {
+      name
+      description
+      instructions
+      category
+      likes
+      createdDate
+    }
   }
-}
+`;
+
+/* Recipes Mutations */
+
+/* User Queries */
+
+/* User Mutations */
+export const SIGNUP_USER = gql`
+  mutation($username: String!, $email: String!, $password: String!) {
+    signUpUser(username: $username, email: $email, password: $password) {
+      token
+    }
+  }
 `;
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Components/App';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import './index.css';
+import App from './Components/App';
+import Signin from './Components/Auth/Signin';
+import Signup from './Components/Auth/Signup';
+
 
 const client = new ApolloClient({
   uri: 'http://localhost:4444/graphql',
 });
 
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route path="/signin" component={Signin} />
+      <Route path="/signup" component={Signup} />
+      <Redirect to="/" />
+    </Switch>
+  </Router>
+);
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <Root />
   </ApolloProvider>,
   document.getElementById('root')
 );
