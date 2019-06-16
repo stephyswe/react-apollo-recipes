5cd7309eda8cf092707a16e864b7d94ec4a8a69b
Stephanie <stephaniiehallberg@gmail.com>
2019-06-16 20:40:23 +0200

    Handle Data for RecipePage

--- client/src/Components/Recipe/AddRecipe.js
+++ client/src/Components/Recipe/AddRecipe.js
@@ -1,7 +1,124 @@
-import React from 'react'
+import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-const AddRecipe = () => (
-  <div>Add Recipe</div>
-)
+import { Mutation } from 'react-apollo';
+import { ADD_RECIPE, GET_ALL_RECIPES } from '../queries';
+import Error from '../Error';
 
-export default AddRecipe;
+const initialState = {
+  name: '',
+  instructions: '',
+  category: 'Breakfast',
+  description: '',
+  username: '',
+};
+
+class AddRecipe extends React.Component {
+  state = { ...initialState };
+
+  clearState = () => {
+    this.setState({ ...initialState });
+  };
+
+  componentDidMount() {
+    console.log(this.props.session.getCurrentUser.username);
+    this.setState({
+      username: this.props.session.getCurrentUser.username,
+    });
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event, addRecipe) => {
+    event.preventDefault();
+    addRecipe().then(({ data }) => {
+      console.log(data);
+      this.clearState();
+      this.props.history.push('/');
+    });
+  };
+
+  validateForm = () => {
+    const { name, category, description, instructions } = this.state;
+    const isInvalid = !name || !category || !description || !instructions;
+    return isInvalid;
+  };
+
+  updateCache = (cache, {data: {addRecipe}}) => {
+    const { getAllRecipes } = cache.readQuery({ query: GET_ALL_RECIPES });
+
+    cache.writeQuery({
+      query: GET_ALL_RECIPES,
+      data: {
+        getAllRecipes: [addRecipe, ...getAllRecipes]
+      }
+    })
+  };
+
+  render() {
+    const { name, category, description, instructions, username } = this.state;
+    return (
+      <Mutation
+        mutation={ADD_RECIPE}
+        variables={{ name, category, description, instructions, username }}
+        update={this.updateCache}
+      >
+        {(addRecipe, { data, loading, error }) => {
+          return (
+            <div className="App">
+              <h2 className="App">Add Recipe</h2>
+              <form
+                className="form"
+                onSubmit={event => this.handleSubmit(event, addRecipe)}
+              >
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Recipe Name"
+                  onChange={this.handleChange}
+                  value={name}
+                />
+                <select
+                  name="category"
+                  onChange={this.handleChange}
+                  value={category}
+                >
+                  <option value="Breakfast">Breakfast</option>
+                  <option value="Lunch">Lunch</option>
+                  <option value="Dinner">Dinner</option>
+                  <option value="Snack">Snack</option>
+                </select>
+                <input
+                  type="text"
+                  name="description"
+                  placeholder="Add description"
+                  onChange={this.handleChange}
+                  value={description}
+                />
+                <textarea
+                  name="instructions"
+                  placeholder="Add instruction"
+                  onChange={this.handleChange}
+                  value={instructions}
+                />
+                <button
+                  disabled={loading || this.validateForm()}
+                  type="submit"
+                  className="button-primary"
+                >
+                  Submit
+                </button>
+                {error && <Error error={error} />}
+              </form>
+            </div>
+          );
+        }}
+      </Mutation>
+    );
+  }
+}
+
+export default withRouter(AddRecipe);
--- client/src/Components/queries/index.js
+++ client/src/Components/queries/index.js
@@ -21,23 +21,50 @@ export const GET_RECIPE = gql`
       instructions
       createdDate
       likes
+      username
     }
   }
 `;
 
 /* Recipes Mutations */
+export const ADD_RECIPE = gql`
+  mutation(
+    $name: String!
+    $description: String!
+    $category: String!
+    $instructions: String!
+    $username: String
+  ) {
+    addRecipe(
+      name: $name
+      description: $description
+      category: $category
+      instructions: $instructions
+      username: $username
+    ) {
+      _id
+      name
+      category
+      description
+      instructions
+      createdDate
+      likes
+      username
+    }
+  }
+`;
 
 /* User Queries */
 
 export const GET_CURRENT_USER = gql`
   query {
-      getCurrentUser {
-        username
-        email
-        joinDate
-      }
+    getCurrentUser {
+      username
+      email
+      joinDate
     }
-`; 
+  }
+`;
 
 /* User Mutations */
 
