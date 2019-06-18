3d3552ac6073f049af6c21e2619b8388c93704be
Stephanie <stephaniiehallberg@gmail.com>
2019-06-18 21:43:14 +0200

    Like / Unlike

--- client/src/Components/Recipe/RecipePage.js
+++ client/src/Components/Recipe/RecipePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Query } from 'react-apollo';
 import { GET_RECIPE } from '../queries';
+import LikeRecipe from './LikeRecipe';
 
 const RecipePage = ({ match }) => {
   const { _id } = match.params;
@@ -11,16 +12,17 @@ const RecipePage = ({ match }) => {
         if (loading) return <div>Loading</div>;
         if (error) return <div>Error</div>;
         console.log(data);
-        return <div className="App">
-          <h2>{data.getRecipe.name}</h2>
-          <p>Category: {data.getRecipe.category}</p>
-          <p>Description: {data.getRecipe.description}</p>
-          <p>Instructions: {data.getRecipe.instructions}</p>
-          <p>Likes: {data.getRecipe.likes}</p>
-          <p>Created By: {data.getRecipe.username}</p>
-          <button>Like</button>
-
-        </div>;
+        return (
+          <div className="App">
+            <h2>{data.getRecipe.name}</h2>
+            <p>Category: {data.getRecipe.category}</p>
+            <p>Description: {data.getRecipe.description}</p>
+            <p>Instructions: {data.getRecipe.instructions}</p>
+            <p>Likes: {data.getRecipe.likes}</p>
+            <p>Created By: {data.getRecipe.username}</p>
+            <LikeRecipe _id={_id} />
+          </div>
+        );
       }}
     </Query>
   );
--- schema.js
+++ schema.js
@@ -34,6 +34,8 @@ type Token {
 type Mutation {
   addRecipe(name: String!, description: String!, category: String!, instructions: String!, username: String): Recipe
   deleteUserRecipe(_id: ID): Recipe
+  likeRecipe(_id: ID! username: String!): Recipe
+  unlikeRecipe(_id: ID! username: String!): Recipe
   signUpUser(username: String!, email: String!, password: String!): Token
   signInUser(username: String!, password: String!): Token
 }
